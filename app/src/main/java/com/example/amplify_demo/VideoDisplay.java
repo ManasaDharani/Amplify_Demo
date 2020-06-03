50bae8157872d2ffaa0ef226796722296666338f
ManasaDG <dgmanasa1@gmail.com>
2020-06-03 15:19:03 -0500

    Delay in Video fixed

--- app/src/main/java/com/example/amplify_demo/VideoDisplay.java
+++ app/src/main/java/com/example/amplify_demo/VideoDisplay.java
@@ -1,8 +1,6 @@
 package com.example.amplify_demo;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,39 +13,8 @@ int i=0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_display);
             videoView=(VideoView)findViewById(R.id.VideoView);
-            videoView.setVideoPath(getApplicationContext().getFilesDir() + "/fileSequence"+0+".ts");
+            videoView.setVideoPath(getApplicationContext().getFilesDir() + "/video1out.mp4");
             videoView.start();
-            setup(videoView);
-            videoView.setOnCompletionListener(completionListener);
     }
-    public void setup(VideoView videoView) {
-        if(i==6) {
-            videoView.stopPlayback();
-            return;
-        }
-        Log.i("MyAmplifyApp",Integer.toString(i));
-        videoView.setVideoPath(getApplicationContext().getFilesDir() + "/fileSequence"+i+".ts");
-        videoView.start();
 
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                int duration = mp.getDuration();
-                int videoDuration = videoView.getDuration();
-                Log.i("MyAmplifyApp-Duration", Integer.toString(videoView.getDuration()));
-            }
-        });
-        i++;
-    }
-    MediaPlayer.OnCompletionListener completionListener=new MediaPlayer.OnCompletionListener() {
-
-        public void onCompletion(MediaPlayer mp) {
-            setup(videoView);
-            if(i==6) {
-                videoView.stopPlayback();
-                mp.stop();
-                return;
-            }
-        }
-    };
 }
--- app/src/main/java/com/example/amplify_demo/VideoLinkActivity.java
+++ app/src/main/java/com/example/amplify_demo/VideoLinkActivity.java
@@ -10,15 +10,19 @@ import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.amplifyframework.core.Amplify;
+import com.arthenica.mobileffmpeg.FFmpeg;
 
 import java.io.File;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+
 public class VideoLinkActivity extends AppCompatActivity {
     VideoView videoView;
     TextView textView;
+
+
     int ind=0;
     private MediaController mediaController;
 
@@ -31,6 +35,16 @@ public class VideoLinkActivity extends AppCompatActivity {
         Log.i("MyAmplifyApp",Integer.toString(video_id));
         int x=0;
         setContentView(R.layout.activity_video_link);
+        //int rc = FFmpeg.execute("-i file1.mp4 -c:v mpeg4 file2.mp4");
+
+
+        Amplify.Storage.downloadFile(
+                "video1.txt",
+                new File(getApplicationContext().getFilesDir() + "/video1.txt"),
+                result -> Log.i("MyAmplifyApp", "Successfully downloaded: " + result.getFile().getName()),
+                error -> Log.e("MyAmplifyApp", "Download Failure", error)
+
+        );
 
             while (x <= 5) {
                 Amplify.Storage.downloadFile(
@@ -42,14 +56,19 @@ public class VideoLinkActivity extends AppCompatActivity {
                 );
                 x++;
             }
+
             final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
             executorService.schedule(task, 10, TimeUnit.SECONDS);
 
 
 
     }
+
+
     Runnable task = new Runnable() {
         public void run() {
+            String cmd="-f concat -safe 0 -i "+getApplicationContext().getFilesDir()+"/video1.txt -c copy "+getApplicationContext().getFilesDir()+"/video1out.mp4";
+            int rc1= FFmpeg.execute(cmd);
             Log.i("MyAmplifyApp","Running PlayVideo procedure");
             Intent intent = new Intent(getApplicationContext(), VideoDisplay.class);
             startActivity(intent);
